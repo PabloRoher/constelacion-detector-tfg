50505a38d10448b2e3728c8613dedd93389b4a2a
Pablo Rodriguez <pablrohe@gmail.com>
2025-04-25 21:40:20 +0200

    Segundo commit - proyecto TFG constelaciones

--- trainMain.py
+++ trainMain.py
@@ -7,9 +7,13 @@ def main():
     # Entrenar el modelo con un dataset personalizado
     train_results = model.train(
         data="C:/Users/pablo/Desktop/TFG/Datasets/Constellation.v1i.yolov11/data.yaml",  
-        epochs=50,
+        epochs=100,
         imgsz=640,
-        device="cuda" 
+        device="cuda",
+        val=True,           # Asegura que haga validación durante el entrenamiento
+        save=True,          # Guarda los mejores modelos
+        save_period=10,     # Guarda cada 10 epochs
+        patience=20         # Early stopping si no mejora tras 20 epochs
     )
 
     # Evaluar el modelo en el conjunto de validación
@@ -21,6 +25,7 @@ def main():
 
     model.export(format="onnx")         # Muy compatible con APIs Python, C++, etc.
     model.export(format="torchscript")  # Rápido y portable en entornos PyTorch
+    model.export(format="openvino") # Para Intel o edge devices
 
 
 if __name__ == "__main__":
